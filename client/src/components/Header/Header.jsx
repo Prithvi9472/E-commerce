53a812ec1af8a750bb5537d3b8f1e07a785dc474
Prithvi9472 <77143389+Prithvi9472@users.noreply.github.com>
2024-05-04 09:02:00 +0530

    Add files via upload

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,61 @@
+import { useEffect, useState, UseContext, useContext } from "react";
+import { useNavigate , Link } from "react-router-dom";
+// import {HashLink} from "react-router-hash-link"
+
+import { TbSearch } from "react-icons/tb";
+import { CgShoppingCart } from "react-icons/cg";
+import { AiOutlineHeart, AiOutlineLogin, AiOutlineProfile, AiOutlineUser } from "react-icons/ai";
+
+import Search from "./Search/Search";
+import Cart from "../Cart/Cart";
+import { Context } from "../../utils/context";
+
+
+
+import "./Header.scss";
+const Header = () => {
+    const [scrolled, setScrolled] = useState(false);
+    const [showCart, setShowCart] = useState(false);
+    const [showSearch, setShowSearch] = useState(false);
+    const {cartCount} = useContext(Context);
+    const navigate = useNavigate()
+    const handleScroll = () => {
+        const offset = window.scrollY;
+        if (offset > 200) {
+            setScrolled(true);
+        }
+        else {
+            setScrolled(false);
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll)
+    }, []);
+
+    return (
+        <>
+            <header className={`main-header ${scrolled ? 'sticky-header' : ""}`} >
+                <div className="header-content">
+                    <ul className="left">
+                        <li onClick={ () => navigate("/")}>Home</li>
+                        <li >About</li>
+                    </ul>
+                    <div className="center" onClick={ () => navigate("/")}>TECHSPO.</div>
+                    <div className="right">
+                        <TbSearch  onClick={() => setShowSearch (true)}/>
+                        <AiOutlineUser/>
+                        <span className="cart-icon" onClick={() => setShowCart (true)}>
+                            <CgShoppingCart />
+                           {!!cartCount && <span>{cartCount}</span>}
+                        </span>
+                    </div>
+                </div>
+            </header>
+            {showCart && <Cart setShowCart= {setShowCart} />}
+            {showSearch && <Search setShowSearch= {setShowSearch} />}
+        </>
+    )
+};
+
+export default Header;
